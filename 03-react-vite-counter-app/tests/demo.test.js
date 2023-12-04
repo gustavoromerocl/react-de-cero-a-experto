57dd60658d37e8944fa7803996d6b93d05937f0e
romerodevelopments <gandresrp@gmail.com>
2023-12-04 07:59:15 -0300

    pruebas unitarias - jest - expect - toBe

--- 03-react-vite-counter-app/tests/demo.test.js
+++ 03-react-vite-counter-app/tests/demo.test.js
@@ -1,5 +1,13 @@
-test('Esta prueba no debe de fallar', () => {
-  if(0 === 1) {
-    throw new Error('No puede dividir entre cero');
-  }
+describe('Pruebas en <DemoComponent />', () => {
+  test('Esta prueba no debe de fallar', () => {
+    // 1. Inicialización
+    const message1 = 'Hola mundo';
+  
+    // 2. Estimulo
+    const message2 = message1.trim();
+  
+    // 3. Observar el comportamiento
+    expect(message1).toBe(message2);
+  })
 })
+
