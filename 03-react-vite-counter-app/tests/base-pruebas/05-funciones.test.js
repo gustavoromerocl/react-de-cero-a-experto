a52bd5669257a861155a95f448cd191b31eabf6d
romerodevelopments <gandresrp@gmail.com>
2023-12-04 21:48:18 -0300

    pruebas unitarias - toEqual

---
+++ 03-react-vite-counter-app/tests/base-pruebas/05-funciones.test.js
@@ -0,0 +1,21 @@
+import { getUser, getUsuarioActivo } from "../../src/base-pruebas/05-funciones"
+
+describe('Pruebas en 05-funciones', () => {
+  test('getUser debe retornar un objeto', () => {
+    const testUser = {
+      uid: 'ABC123',
+      username: 'El_Papi1502'
+    }
+
+    const user = getUser();
+
+    expext(testUser).toEqual(user);
+  })
+
+  describe('GetUsuarioActivo debe retornar un objeto', () => {
+    const name = 'Gustavo'
+
+    const user = getUsuarioActivo(name)
+    expect(user.name).toStrictEqual(name);
+  })
+})
