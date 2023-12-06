af4cddb91ef762bdf8abe70e05762cebc4955977
romerodevelopments <gandresrp@gmail.com>
2023-12-06 08:29:05 -0300

    pruebas unitarias - getByTestId y otras propiedades

--- 03-react-vite-counter-app/tests/FirstApp.test.jsx
+++ 03-react-vite-counter-app/tests/FirstApp.test.jsx
@@ -10,9 +10,22 @@ describe('Pruebas en <FirstApp />', () => {
 
   test('debe mostrar el título en un h1', () => {
     const title = 'Hola soy gustavo';
-    const { container, getByText } = render(<FirstApp title={title} />);
+    const { container, getByText, getByTestId } = render(<FirstApp title={title} />);
     expect(getByText(title)).toBeTruthy();
-    const h1 = container.querySelector('h1');
-    expect(h1.innerHTML).toContain(title);
+    // const h1 = container.querySelector('h1');
+    // expect(h1.innerHTML).toContain(title);
+
+    expect(getByTestId('test-title').innerHTML).toContain(title);
+  })
+
+  test('debe mostrar el subtitulo enviado por props', () => {
+    const title = 'Hola soy gustavo';
+    const subTitle = 'Soy un subtitulo'
+    const { getAllByText } = render(
+    <FirstApp 
+      title={title}
+      subTitle={subTitle} 
+      />);
+    expect(getAllByText(subTitle).length).toBe(2);
   })
 })
