108030af8bba6c97e82ce738d6f18cb9fdf05371
romerodevelopments <gandresrp@gmail.com>
2024-01-13 11:48:23 -0300

    journal app - material ui: SideBar

--- 08-journal-app/src/journal/layout/JournalLayout.jsx
+++ 08-journal-app/src/journal/layout/JournalLayout.jsx
@@ -1,5 +1,6 @@
-import { Box } from "@mui/material"
+import { Box, Toolbar } from "@mui/material"
 import { NavBar } from "../components/NavBar";
+import { SideBar } from "../components/SideBar";
 
 const drawerWidth = 240;
 
@@ -7,11 +8,12 @@ export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: 'flex'}}>
       <NavBar drawerWidth={drawerWidth}/>
-      {/* Sidebar */}
+      <SideBar drawerWidth={drawerWidth}/>
       <Box
         component='main'
         sx={{ flexGrow: 1, p: 3 }}
       >
+        <Toolbar/>
         { children }
       </Box>
     </Box>
